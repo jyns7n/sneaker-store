f6b98711fbbb3062c0282cba1f5250f819eda788
ertpc <ertpc1997@gmail.com>
2023-08-30 00:33:53 +0700

    add product section and quality product section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,14 @@
 const Button = ({ label, iconURL }) => {
   return (
-    <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white'>
+    <button className="flex items-center justify-center gap-2 rounded-full border bg-coral-red px-7 py-4 font-montserrat text-lg leading-none text-white">
       {label}
-      <img
-        src={iconURL}
-        alt='arrow right icon'
-        className='ml-2 rounded-full w-5 h-5'
-      />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="ml-2 h-5 w-5 rounded-full"
+        />
+      )}
     </button>
   );
 };
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,19 @@
-import { navLinks } from '../constants';
-import { headerLogo } from '../assets/images';
-import { hamburger } from '../assets/icons';
+import { navLinks } from "../constants";
+import { headerLogo } from "../assets/images";
+import { hamburger } from "../assets/icons";
 
 const Nav = () => {
   return (
-    <header className='padding-x py-8 absolute z-10 w-full'>
-      <nav className='flex justify-between items-center max-container'>
-        <a href='/'>
-          <img src={headerLogo} alt='Logo' width={130} height={29} />
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="Logo" width={130} height={29} />
         </a>
-        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
           {navLinks.map((link) => (
             <li key={link.label}>
               <a
-                className='font-montserrat leading-normal text-lg text-slate-gray'
+                className="font-montserrat leading-normal text-lg text-slate-gray"
                 href={link.href}
               >
                 {link.label}
@@ -21,8 +21,8 @@ const Nav = () => {
             </li>
           ))}
         </ul>
-        <div className='hidden max-lg:block'>
-          <img src={hamburger} alt='hamburger' width={25} height={25} />
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="hamburger" width={25} height={25} />
         </div>
       </nav>
     </header>
